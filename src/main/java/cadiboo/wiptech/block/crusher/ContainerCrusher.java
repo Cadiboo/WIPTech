87d56608eec7db38a1e2f013e20451984a5048dd
Cadiboo <greythunder01@gmail.com>
2018-03-13 23:41:50 +1100

    Updated ContainerCrusher

--- src/main/java/cadiboo/wiptech/block/crusher/ContainerCrusher.java
+++ src/main/java/cadiboo/wiptech/block/crusher/ContainerCrusher.java
@@ -1,108 +1,118 @@
 package cadiboo.wiptech.block.crusher;
 
-import javax.annotation.Nonnull;
-
 import cadiboo.wiptech.init.Items;
+import cadiboo.wiptech.init.Recipes;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Nonnull;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.NonNullList;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
 
-public class ContainerCrusher extends Container {
-
-	private void addSlotToContainerCrusher(TileEntityCrusher crusher, int index, int xPos, int yPos)
+public class ContainerCrusher
+extends Container
+{
+	private void addSlotToContainerCrusher(final TileEntityCrusher crusher, final int index, int xPos, int yPos)
 	{
-		IItemHandler inventory = crusher.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
-		addSlotToContainer(new SlotItemHandler(inventory, index, xPos, yPos) {
-			@Override
-			public void onSlotChanged() {
+		IItemHandler inventory = (IItemHandler)crusher.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
+		addSlotToContainer(new SlotItemHandler(inventory, index, xPos, yPos)
+		{
+			public void onSlotChanged()
+			{
 				crusher.markDirty();
-			};
-			@Override
-			public boolean isItemValid(@Nonnull ItemStack stack){
-				//dont allow anything to be put i
-				return false;
+			}
+
+			public boolean isItemValid(@Nonnull ItemStack stack)
+			{
+				boolean returnResult = false;
+
+				ItemStack slot0Stack = ((IItemHandler)crusher.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH)).getStackInSlot(0);
+				if (slot0Stack.getItem() == Items.CRUSHER_BIT) {
+					for (ItemStack result : Recipes.getCrushResults(index - 1)) {
+						if ((!returnResult) && (result != null) && (result.getItem() == stack.getItem())) {
+							returnResult = true;
+						}
+					}
+				} else if (slot0Stack.getItem() == Items.HAMMER) {
+					for (ItemStack result : Recipes.getHammerResults(index - 1)) {
+						if ((!returnResult) && (result != null) && (result.getItem() == stack.getItem())) {
+							returnResult = true;
+						}
+					}
+				}
+				return returnResult;
 			}
 		});
 	}
 
+	public ContainerCrusher(InventoryPlayer playerInv, final TileEntityCrusher crusher)
+	{
+		IItemHandler inventory = (IItemHandler)crusher.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
 
-	public ContainerCrusher(InventoryPlayer playerInv, final TileEntityCrusher crusher) {
-		IItemHandler inventory = crusher.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
-
-		//block inventory
-		//addSlotToContainerCrusher(crusher, 0, 24, 17); //crusher bit
-		addSlotToContainer(new SlotItemHandler(inventory, 0, 24, 17) {
-			@Override
-			public void onSlotChanged() {
+		addSlotToContainer(new SlotItemHandler(inventory, 0, 24, 17)
+		{
+			public void onSlotChanged()
+			{
 				crusher.markDirty();
 			}
-			@Override
-			public boolean isItemValid(@Nonnull ItemStack stack){
-				//return stack.getItem() instanceof ItemCrusherBit;
-				if (stack.getItem() == Items.CRUSHER_BIT)
+
+			public boolean isItemValid(@Nonnull ItemStack stack)
+			{
+				if ((stack.getItem() == Items.CRUSHER_BIT) || (stack.getItem() == Items.HAMMER)) {
 					return true;
-				else
-					return false;
+				}
+				return false;
 			}
-			@Override
+
 			public int getSlotStackLimit()
 			{
 				return 1;
 			}
 		});
-		//addSlotToContainerCrusher(crusher, 1, 24, 53); //item to crush
-		addSlotToContainer(new SlotItemHandler(inventory, 1, 24, 53) {
-			@Override
-			public void onSlotChanged() {
+		addSlotToContainer(new SlotItemHandler(inventory, 1, 24, 53)
+		{
+			public void onSlotChanged()
+			{
 				crusher.markDirty();
 			}
-			/*@Override
-			public boolean isItemValid(@Nonnull ItemStack stack){
-				return getCrushResult(stack.getItem());
-			}*/
-		});
-		addSlotToContainerCrusher(crusher, 2, 83, 19); //product 1 (gravel)
-		addSlotToContainerCrusher(crusher, 3, 114, 19); //product 2 (alumina)
-		addSlotToContainerCrusher(crusher, 4, 145, 19); //product 3 (Iron Oxides)
-		addSlotToContainerCrusher(crusher, 5, 83, 51); //product 4 (Silica)
-		addSlotToContainerCrusher(crusher, 6, 114, 51); //product 5 (Titania)
-		addSlotToContainerCrusher(crusher, 7, 145, 51); //product 6 (Gallium)
 
-		//addSlotToContainerCrusher(crusher, 0, 80, 35); //centre
-		/*addSlotToContainer(new SlotItemHandler(inventory, 0, 20, 15) {
-			@Override
-			public void onSlotChanged() {
-				crusher.markDirty();
+			public boolean isItemValid(@Nonnull ItemStack stack)
+			{
+				ItemStack slot0Stack = ((IItemHandler)crusher.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH)).getStackInSlot(0);
+				if (slot0Stack.getItem() == Items.CRUSHER_BIT) {
+					return (Recipes.getCrushResult(stack) != null) && (Recipes.getCrushResult(stack).size() > 0);
+				}
+				if (slot0Stack.getItem() == Items.HAMMER) {
+					return (Recipes.getHammerResult(stack) != null) && (Recipes.getHammerResult(stack).size() > 0);
+				}
+				return false;
 			}
 		});
-		addSlotToContainer(new SlotItemHandler(inventory, 1, 80, 35) {
-			@Override
-			public void onSlotChanged() {
-				crusher.markDirty();
-			}
-		});*/
-
-		//player inventory
+		addSlotToContainerCrusher(crusher, 2, 83, 19);
+		addSlotToContainerCrusher(crusher, 3, 114, 19);
+		addSlotToContainerCrusher(crusher, 4, 145, 19);
+		addSlotToContainerCrusher(crusher, 5, 83, 51);
+		addSlotToContainerCrusher(crusher, 6, 114, 51);
+		addSlotToContainerCrusher(crusher, 7, 145, 51);
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 9; j++) {
 				addSlotToContainer(new Slot(playerInv, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
 			}
 		}
-
-		//player hotbar
 		for (int k = 0; k < 9; k++) {
 			addSlotToContainer(new Slot(playerInv, k, 8 + k * 18, 142));
 		}
 	}
 
-	@Override
-	public boolean canInteractWith(EntityPlayer player) {
+	public boolean canInteractWith(EntityPlayer player)
+	{
 		return true;
 	}
 
