86ff03e263e64383bbbcc22830b66cc8abcda27b
Cadiboo <greythunder01@gmail.com>
2018-03-19 19:24:13 +1100

    chance that I got this right is very small

--- src/main/java/cadiboo/wiptech/client/render/RenderEntityFerromagneticProjectile.java
+++ src/main/java/cadiboo/wiptech/client/render/RenderEntityFerromagneticProjectile.java
@@ -1,5 +1,7 @@
 package cadiboo.wiptech.client.render;
 
+import java.util.List;
+
 import javax.annotation.Nonnull;
 
 import org.lwjgl.opengl.GL11;
@@ -8,18 +10,31 @@ import org.lwjgl.opengl.GL12;
 import cadiboo.wiptech.WIPTech;
 import cadiboo.wiptech.entity.projectile.EntityFerromagneticProjectile;
 import cadiboo.wiptech.init.Items;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockShulkerBox;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.block.model.ModelManager;
+import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.renderer.texture.TextureUtil;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.client.renderer.color.ItemColors;
 
 
 //COPPIED FROM IMMERSIVE ENGINEERING
@@ -40,15 +55,49 @@ public class RenderEntityFerromagneticProjectile<T extends EntityFerromagneticPr
 		super(renderManager);
 	}
 
+	private ItemStack itemStack = new ItemStack(Items.FERROMAGNETIC_PROJECILE);
+
 	@Override
 	public void doRender(@Nonnull T entity, double x, double y, double z, float f0, float f1)
 	{
+		itemStack = entity.getAmmoStack();
+
 		GlStateManager.pushMatrix();
-		this.bindEntityTexture(entity);
+		//this.bindEntityTexture(entity);
+		this.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
 		GlStateManager.translate(x, y, z);
 		GlStateManager.enableRescaleNormal();
 		Tessellator tessellator = Tessellator.getInstance();
 		BufferBuilder bufferbuilder = tessellator.getBuffer();
+
+		/*if(!itemStack.isEmpty()) {
+			Minecraft.getMinecraft().getRenderItem().renderItem(itemStack, ItemCameraTransforms.TransformType.NONE);
+		}
+		else {
+			ItemStack dummy = ItemStack.EMPTY;
+			Minecraft.getMinecraft().getRenderItem().renderItem(dummy, Minecraft.getMinecraft().getRenderItem().getItemModelMesher().getModelManager().getMissingModel());
+		}*/
+
+		/*
+		 else if (Block.getBlockFromItem(item) instanceof BlockShulkerBox)
+	        {
+	            TileEntityRendererDispatcher.instance.render(SHULKER_BOXES[BlockShulkerBox.getColorFromItem(item).getMetadata()], 0.0D, 0.0D, 0.0D, 0.0F, partialTicks);
+	        }
+		 */
+
+		ItemStack stack = itemStack;
+		int color = -1;
+		IBakedModel model = Minecraft.getMinecraft().getRenderItem().getItemModelMesher().getItemModel(itemStack);
+
+		bufferbuilder.begin(7, DefaultVertexFormats.ITEM);
+
+		for (EnumFacing enumfacing : EnumFacing.values())
+		{
+			this.renderQuads(bufferbuilder, model.getQuads((IBlockState)null, enumfacing, 0L), color, stack);
+		}
+
+		this.renderQuads(bufferbuilder, model.getQuads((IBlockState)null, (EnumFacing)null, 0L), color, stack);
+		tessellator.draw();
 
 		GlStateManager.disableCull();
 		GlStateManager.rotate(entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * f1 - 90.0F, 0.0F, 1.0F, 0.0F);
@@ -89,5 +138,31 @@ public class RenderEntityFerromagneticProjectile<T extends EntityFerromagneticPr
 	@Override
 	protected ResourceLocation getEntityTexture(@Nonnull T entity) {
 		return TextureMap.LOCATION_MISSING_TEXTURE;
+	}
+
+	private void renderQuads(BufferBuilder renderer, List<BakedQuad> quads, int color, ItemStack stack)
+	{
+		boolean flag = color == -1 && !stack.isEmpty();
+		int i = 0;
+
+		for (int j = quads.size(); i < j; ++i)
+		{
+			BakedQuad bakedquad = quads.get(i);
+			int k = color;
+
+			if (flag && bakedquad.hasTintIndex())
+			{
+				k = Minecraft.getMinecraft().getItemColors().colorMultiplier(stack, bakedquad.getTintIndex());
+
+				if (EntityRenderer.anaglyphEnable)
+				{
+					k = TextureUtil.anaglyphColor(k);
+				}
+
+				k = k | -16777216;
+			}
+
+			net.minecraftforge.client.model.pipeline.LightUtil.renderQuadColor(renderer, bakedquad, k);
+		}
 	}
 }
