ff2c2d0acc81fc27cceef734219ed061efb14739
Cadiboo <greythunder01@gmail.com>
2018-04-14 08:48:22 +1000

    TestModularWeapon no longer crashes the game

--- src/main/java/cadiboo/wiptech/item/ItemTestModularWeapon.java
+++ src/main/java/cadiboo/wiptech/item/ItemTestModularWeapon.java
@@ -1,57 +1,77 @@
 package cadiboo.wiptech.item;
 
-import cadiboo.wiptech.WIPTech;
-import cadiboo.wiptech.capability.IWeaponModular;
-import cadiboo.wiptech.capability.WeaponModular;
-import cadiboo.wiptech.handler.EnumHandler;
-import cadiboo.wiptech.handler.EnumHandler.WeaponModules.Circuits;
-import cadiboo.wiptech.handler.EnumHandler.WeaponModules.Coils;
-import cadiboo.wiptech.handler.EnumHandler.WeaponModules.Scopes;
-import cadiboo.wiptech.init.Capabilities;
-import cadiboo.wiptech.init.Capabilities.CapabilityWeaponModular;
+import cadiboo.wiptech.entity.projectile.EntityFerromagneticProjectile;
 import cadiboo.wiptech.init.Items;
 import cadiboo.wiptech.provider.ModularWeaponProvider;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 
 public class ItemTestModularWeapon extends ItemBase {
 
-	//private final IWeaponModular modules = null;
+	// private final IWeaponModular modules = null;
+	private static final ItemStack PROJECTILE_STACK = new ItemStack(Items.FERROMAGNETIC_PROJECILE, 1, 2);
 
 	public ItemTestModularWeapon(String name) {
 		super(name);
 	}
 
-	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
-	{
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
 		ItemStack itemstack = playerIn.getHeldItem(handIn);
 
-		if( itemstack.getItem() == Items.TEST_MODULAR_WEAPON ) {
-			WIPTech.logger.info(itemstack.getCapability(Capabilities.MODULAR_WEAPON_CAPABILITY, null).getModules());
-			WIPTech.logger.info(itemstack.getCapability(Capabilities.MODULAR_WEAPON_CAPABILITY, null).getModuleList());
-			int circuit = itemstack.getCapability(Capabilities.MODULAR_WEAPON_CAPABILITY, null).getCircuit().getID();
-			circuit++;
-			if(circuit>2)
-				circuit=0;
-			itemstack.getCapability(Capabilities.MODULAR_WEAPON_CAPABILITY, null).setCircuit(Circuits.byID(circuit));
+		if (itemstack.getItem() == Items.TEST_MODULAR_WEAPON) {
+			// WIPTech.logger.info(itemstack.getCapability(Capabilities.MODULAR_WEAPON_CAPABILITY,
+			// null).getModules());
+			// WIPTech.logger.info(itemstack.getCapability(Capabilities.MODULAR_WEAPON_CAPABILITY,
+			// null).getModuleList());
+			// int circuit = itemstack.getCapability(Capabilities.MODULAR_WEAPON_CAPABILITY,
+			// null).getCircuit().getID();
+			// circuit++;
+			// if(circuit>2)
+			// circuit=0;
+			// itemstack.getCapability(Capabilities.MODULAR_WEAPON_CAPABILITY,
+			// null).setCircuit(Circuits.byID(circuit));
 		}
-
 		playerIn.setActiveHand(handIn);
 		return new ActionResult(EnumActionResult.SUCCESS, itemstack);
 	}
 
 	@Override
-	public ICapabilityProvider initCapabilities( ItemStack item, NBTTagCompound nbt ) {
-		if( item.getItem() == Items.TEST_MODULAR_WEAPON ) {
+	public ICapabilityProvider initCapabilities(ItemStack item, NBTTagCompound nbt) {
+		if (item.getItem() == Items.TEST_MODULAR_WEAPON) {
 			return new ModularWeaponProvider();
 		}
 		return null;
+	}
+
+	@Override
+	public void onUsingTick(ItemStack itemStackIn, EntityLivingBase entityLiving, int count) {
+		float velocity = 0;
+		World worldIn = entityLiving.getEntityWorld();
+		EntityPlayer player = (EntityPlayer) entityLiving;
+
+		if (!worldIn.isRemote) {
+
+			EntityFerromagneticProjectile projectile = ((ItemFerromagneticProjectile) PROJECTILE_STACK.getItem())
+					.createProjectile(worldIn, PROJECTILE_STACK, player, true);
+			velocity = EntityFerromagneticProjectile.getProjectileVelocity(PROJECTILE_STACK);
+			projectile.setDamage(projectile.getDamage());
+			projectile.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, velocity, 0.1F);
+
+			worldIn.spawnEntity(projectile);
+		}
+
+		worldIn.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_FIREWORK_SHOOT,
+				SoundCategory.PLAYERS, 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + velocity * 0.5F);
 	}
 
 }
