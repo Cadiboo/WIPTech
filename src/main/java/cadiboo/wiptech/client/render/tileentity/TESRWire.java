0cc0f84dc2c3e938bc9952bb130e2a605ccc038e
Cadiboo <greythunder01@gmail.com>
2018-05-01 23:00:35 +1000

    cut down code & improved visuals when connecting to non-wire blocks

--- src/main/java/cadiboo/wiptech/client/render/tileentity/TESRWire.java
+++ src/main/java/cadiboo/wiptech/client/render/tileentity/TESRWire.java
@@ -6,18 +6,20 @@ import cadiboo.wiptech.client.model.ModelEnamel;
 import cadiboo.wiptech.client.model.ModelWire;
 import cadiboo.wiptech.tileentity.TileEntityWire;
 import cadiboo.wiptech.util.Reference;
-import cadiboo.wiptech.util.Utils;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+
+//Rotation algorithm Taken off Max_the_Technomancer from
+// www.minecraftforum.net/forums/mapping-and-modding-java-edition/minecraft-mods/modification-development/2772267-tesr-getting-darker-and-lighter-as-it-rotates
 
 public class TESRWire extends TileEntitySpecialRenderer<TileEntityWire> {
 
 	private static final ResourceLocation ENAMEL_TEXTURE = new ResourceLocation("minecraft", "textures/blocks/concrete_brown.png");
+
+	private static final float	ONE_SIXTEENTH		= 0.0625F;
+	private static final float	SEVEN_SIXTEENTHS	= 0.4375F;
 
 	private static final ModelWire	DOWN_MODEL_WIRE		= new ModelWire();
 	private static final ModelWire	UP_MODEL_WIRE		= new ModelWire();
@@ -36,113 +38,31 @@ public class TESRWire extends TileEntitySpecialRenderer<TileEntityWire> {
 	@Override
 	public void render(TileEntityWire tileEntity, double x, double y, double z, float partialTick, int destroyStage, float alpha) {
 
-		TextureAtlasSprite sprite = Utils.getSpriteFromItemStack(new ItemStack(tileEntity.getBlockType()));
-		if (sprite == null) {
-			WIPTech.logger.info("Sprite is null!");
+		if (!(tileEntity.getBlockType() instanceof BlockWire)) {
+			WIPTech.logger.error("WIRE RENDERING ERROR! BLOCK IS NOT WIRE");
 			return;
 		}
+		final ResourceLocation texLoc = new ResourceLocation(Reference.ID, "textures/items/" + ((BlockWire) tileEntity.getBlockType()).getMetal().getName() + "_wire.png");
+		final boolean isEnamel = ((BlockWire) tileEntity.getBlockType()).isEnamel();
 
-		float minU = sprite.getMinU();
-		float maxU = sprite.getMaxU();
-		float minV = sprite.getMinV();
-		float maxV = sprite.getMaxV();
+		sides: for (int i = 0; i < EnumFacing.VALUES.length; i++) {
+			final EnumFacing side = EnumFacing.VALUES[i];
+			if (!tileEntity.isConnectedTo(side))
+				continue sides;
+			GlStateManager.pushMatrix();
+			GlStateManager.translate(x + 0.5, y + 0.5, z + 0.5);
+			GlStateManager.rotate(side == EnumFacing.DOWN ? 0 : side == EnumFacing.UP ? 180F : side == EnumFacing.NORTH || side == EnumFacing.EAST ? 90F : -90F,
+					side.getAxis() == EnumFacing.Axis.Z ? 1 : 0, 0, side.getAxis() == EnumFacing.Axis.Z ? 0 : 1);
+			GlStateManager.rotate(-90, 0, 0, 1);
 
-		float width = maxU - minU;
-		float height = maxV - minV;
-
-		float midU = minU + (width / 2);
-		float midV = minV + (height / 2);
-
-		float multiplier = 2;
-
-		minU = midU;
-		maxU = midU + (width / 16);
-		minV = midV - (height / 16);
-		maxV = midV;
-
-		ResourceLocation texLoc = TextureMap.LOCATION_BLOCKS_TEXTURE;
-		boolean isEnamel = false;
-
-		if (tileEntity.getBlockType() instanceof BlockWire) {
-			texLoc = new ResourceLocation(Reference.ID, "textures/items/" + ((BlockWire) tileEntity.getBlockType()).getMetal().getName() + "_wire.png");
-			isEnamel = ((BlockWire) tileEntity.getBlockType()).isEnamel();
-		} else
-			WIPTech.logger.error("WIRE RENDERING ERROR!!!!");
-
-		this.bindTexture(texLoc);
-
-		GlStateManager.pushMatrix();
-		GlStateManager.translate(x + 0.5, y + 0.5, z + 0.5);
-		setLightmapDisabled(false);
-
-		/* Why upside down? because UV */
-		GlStateManager.rotate(180, 0F, 0F, 1F);
-
-		if (tileEntity.isConnectedTo(EnumFacing.DOWN)) {
-			GlStateManager.rotate(180, 1F, 1F, 0F);
-			DOWN_MODEL_WIRE.render(0.0625F);
 			if (isEnamel) {
 				this.bindTexture(ENAMEL_TEXTURE);
-				DOWN_MODEL_ENAMEL.render(0.0625F);
+				DOWN_MODEL_ENAMEL.render(ONE_SIXTEENTH);
+				GlStateManager.translate(SEVEN_SIXTEENTHS, 0, 0);
 				this.bindTexture(texLoc);
 			}
-			GlStateManager.rotate(-180, 1F, 1F, 0F);
+			DOWN_MODEL_WIRE.render(ONE_SIXTEENTH);
+			GlStateManager.popMatrix();
 		}
-
-		if (tileEntity.isConnectedTo(EnumFacing.UP)) {
-			GlStateManager.rotate(270, 0F, 0F, 1F);
-			UP_MODEL_WIRE.render(0.0625F);
-			if (isEnamel) {
-				this.bindTexture(ENAMEL_TEXTURE);
-				UP_MODEL_ENAMEL.render(0.0625F);
-				this.bindTexture(texLoc);
-			}
-			GlStateManager.rotate(-270, 0F, 0F, 1F);
-		}
-
-		if (tileEntity.isConnectedTo(EnumFacing.NORTH)) {
-			GlStateManager.rotate(90, 0F, 1F, 0F);
-			NORTH_MODEL_WIRE.render(0.0625F);
-			if (isEnamel) {
-				this.bindTexture(ENAMEL_TEXTURE);
-				NORTH_MODEL_ENAMEL.render(0.0625F);
-				this.bindTexture(texLoc);
-			}
-			GlStateManager.rotate(-90, 0F, 1F, 0F);
-		}
-
-		if (tileEntity.isConnectedTo(EnumFacing.SOUTH)) {
-			GlStateManager.rotate(180, 1F, 0F, 1F);
-			SOUTH_MODEL_WIRE.render(0.0625F);
-			if (isEnamel) {
-				this.bindTexture(ENAMEL_TEXTURE);
-				SOUTH_MODEL_ENAMEL.render(0.0625F);
-				this.bindTexture(texLoc);
-			}
-			GlStateManager.rotate(-180, 1F, 0F, 1F);
-		}
-
-		if (tileEntity.isConnectedTo(EnumFacing.WEST)) {
-			WEST_MODEL_WIRE.render(0.0625F);
-			if (isEnamel) {
-				this.bindTexture(ENAMEL_TEXTURE);
-				WEST_MODEL_ENAMEL.render(0.0625F);
-				this.bindTexture(texLoc);
-			}
-		}
-
-		if (tileEntity.isConnectedTo(EnumFacing.EAST)) {
-			GlStateManager.rotate(180, 0F, 0F, 1F);
-			EAST_MODEL_WIRE.render(0.0625F);
-			if (isEnamel) {
-				this.bindTexture(ENAMEL_TEXTURE);
-				EAST_MODEL_ENAMEL.render(0.0625F);
-				this.bindTexture(texLoc);
-			}
-			GlStateManager.rotate(-180, 0F, 0F, 1F);
-		}
-
-		GlStateManager.popMatrix();
 	}
-
 }
