369dc7423a1d7d62f3b978e92572dfabf1341878
Cadiboo <greythunder01@gmail.com>
2018-05-07 20:46:35 +1000

    scaled lightnigh done right ended up not being what I wanted

--- src/main/java/cadiboo/wiptech/client/render/tileentity/TESRWire.java
+++ src/main/java/cadiboo/wiptech/client/render/tileentity/TESRWire.java
@@ -1,6 +1,5 @@
 package cadiboo.wiptech.client.render.tileentity;
 
-import java.util.List;
 import java.util.Random;
 
 import cadiboo.wiptech.WIPTech;
@@ -14,8 +13,6 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 
@@ -60,181 +57,115 @@ public class TESRWire extends TileEntitySpecialRenderer<TileEntityWire> {
 		if (tileEntity.electrocutionTime > 0) {
 			Tessellator tessellator = Tessellator.getInstance();
 			BufferBuilder bufferbuilder = tessellator.getBuffer();
+			GlStateManager.pushMatrix();
 			GlStateManager.disableTexture2D();
 			GlStateManager.disableLighting();
 			GlStateManager.enableBlend();
 			GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE);
+			GlStateManager.translate(x, y, z);
 
-			List<Entity> entities = tileEntity.getAllEntitiesWithinRangeAt(tileEntity.getPos().getX(), tileEntity.getPos().getY(), tileEntity.getPos().getZ(), 3);
-			for (int i = 0; i < entities.size(); i++) {
-				GlStateManager.pushMatrix();
-				GlStateManager.translate(x + 0.5, y + 0.5, z + 0.5);
+			final int number = 5;
+			final int NumberOfBranches = 1;
+			final int NumberOfPossibleSubBranches = 2;
+			final double scale = 0.5;
+			final double scale16 = scale / 16;
 
-				double flyingMultiplier = 1.825;
-				double yFlying = 1.02;
-				double yAdd = 0.0784000015258789;
+			double[] translateXArray = new double[8];
+			double[] translateZArray = new double[8];
+			double tempX = 0.0D;
+			double tempZ = 0.0D;
+			Random random = new Random(number);
 
-				if (entities.get(i) instanceof EntityPlayer) {
-					if (((EntityPlayer) entities.get(i)).capabilities.isFlying) {
-						flyingMultiplier = 1.1;
-						yFlying = 1.67;
-						yAdd = 0;
-					}
-				}
+			for (int counter_ = 7; counter_ >= 0; --counter_) {
+				translateXArray[counter_] = tempX;
+				translateZArray[counter_] = tempZ;
+				tempX += random.nextInt(11) - 5;
+				tempZ += random.nextInt(11) - 5;
+			}
 
-				double yGround = entities.get(i).motionY + yAdd == 0 && entities.get(i).prevPosY > entities.get(i).posY ? entities.get(i).posY - entities.get(i).prevPosY : 0;
-				double xFall = 1;
-				if (flyingMultiplier == 1.825) {
-					if (entities.get(i).motionX != 0) {
-						if (entities.get(i).motionY + yAdd != 0) {
-							xFall = 0.6;
-						} else if (yGround != 0) {
-							xFall = 0.6;
-						}
-					} else {
-						xFall = 0.6;
-					}
-				}
+			for (int shells = 0; shells < 5; ++shells) {
+				Random random1 = new Random(number);
+				for (int branches = 0; branches < NumberOfBranches; branches++) {
+					for (int possibleSubBranches = 0; possibleSubBranches < NumberOfPossibleSubBranches + 1; ++possibleSubBranches) {
+						int position = 7;
+						int decendingHeight = 0;
 
-				double zFall = 1;
-				if (flyingMultiplier == 1.825) {
-					if (entities.get(i).motionZ != 0) {
-						if (entities.get(i).motionY + yAdd != 0) {
-							zFall = 0.6;
-						} else if (yGround != 0) {
-							zFall = 0.6;
-						}
-					} else {
-						zFall = 0.6;
-					}
-				}
-
-				double dX = entities.get(i).posX - (entities.get(i).prevPosX - entities.get(i).posX) * partialTicks - (entities.get(i).motionX * xFall) * flyingMultiplier - tileEntity.getPos().getX()
-						- 0.5;
-				double dY = entities.get(i).posY - yGround - (entities.get(i).prevPosY - entities.get(i).posY) * partialTicks - (entities.get(i).motionY + yAdd) * yFlying - tileEntity.getPos().getY()
-						- 0.5;
-				double dZ = entities.get(i).posZ - (entities.get(i).prevPosZ - entities.get(i).posZ) * partialTicks - (entities.get(i).motionZ * zFall) * flyingMultiplier - tileEntity.getPos().getZ()
-						- 0.5;
-				dY += entities.get(i).getEyeHeight();
-
-				double yAngle = Math.atan2(0 - dX, 0 - dZ);
-				yAngle = yAngle * (180 / Math.PI);
-				yAngle = yAngle < 0 ? 360 - (-yAngle) : yAngle;
-				// GlStateManager.rotate((float) yAngle + 90, 0, 1, 0);
-				// GlStateManager.rotate(180, 1, 0, 0);
-
-				double _Angle = Math.atan2(dY, Math.sqrt(dX * dX + dZ * dZ));
-				_Angle = _Angle * (180 / Math.PI);
-				_Angle = _Angle < 0 ? 360 - (-_Angle) : _Angle;
-				// GlStateManager.rotate(90 - (float) _Angle, 0, 0, 1);
-
-				// final double scale = 0.125 * Math.sqrt(dX * dX + dY * dY + dZ * dZ);
-				final double scale = 16;
-				final double scale16 = scale / 16;
-
-				// GlStateManager.translate(-3.5 * scale / 16, -8 * scale, -4 * scale / 16);
-
-				Random random = new Random(RANDOMS[(int) (getWorld().getTotalWorldTime() % RANDOMS.length)]);
-
-				final int NumberOfBranches = 1;
-				final int NumberOfPossibleSubBranches = 2;
-
-				double[] adouble = new double[8];
-				double[] adouble1 = new double[8];
-				double d0 = 0.0D;
-				double d1 = 0.0D;
-
-				for (int __ = 7; __ >= 0; --__) {
-					adouble[__] = d0;
-					adouble1[__] = d1;
-					d0 += random.nextInt(11) - 5;
-					d1 += random.nextInt(11) - 5;
-				}
-
-				for (int k1 = 0; k1 < 4; ++k1) {
-					Random random1 = random;
-
-					for (int j = 0; j < 3; ++j) {
-						int k = 7;
-						int l = 0;
-
-						if (j > 0) {
-							k = 7 - j;
+						if (possibleSubBranches > 0) {
+							position = 7 - possibleSubBranches;
 						}
 
-						if (j > 0) {
-							l = k - 2;
+						if (possibleSubBranches > 0) {
+							decendingHeight = position - 2;
 						}
 
-						double d2 = adouble[k] - d0;
-						double d3 = adouble1[k] - d1;
+						double topTranslateX = translateXArray[position] - tempX;
+						double topTranslateZ = translateZArray[position] - tempZ;
 
-						for (int i1 = k; i1 >= l; --i1) {
-							double d4 = d2;
-							double d5 = d3;
+						for (int yPos = position; yPos >= decendingHeight; --yPos) {
+							double bottomTranslateX = topTranslateX;
+							double bottomTranslateZ = topTranslateZ;
 
-							if (j == 0) {
-								d2 += random1.nextInt(11) - 5;
-								d3 += random1.nextInt(11) - 5;
-							} else {
-								d2 += random1.nextInt(31) - 15;
-								d3 += random1.nextInt(31) - 15;
+							if (possibleSubBranches == 0) { // Main branch
+								topTranslateX += random1.nextInt(11) - 5;
+								topTranslateZ += random1.nextInt(11) - 5;
+							} else {// Sub branch
+								topTranslateX += random1.nextInt(31) - 15;
+								topTranslateZ += random1.nextInt(31) - 15;
 							}
 
 							bufferbuilder.begin(5, DefaultVertexFormats.POSITION_COLOR);
-							float f = 0.5F;
-							float f1 = 0.45F;
-							float f2 = 0.45F;
-							float f3 = 0.5F;
-							double d6 = 0.1D + k1 * 0.2D;
+							double topWidth = 0.1D + shells * 0.2D;
 
-							if (j == 0) {
-								d6 *= i1 * 0.1D + 1.0D;
+							if (yPos == 0) {
+								topWidth *= yPos * 0.1D + 1.0D;
 							}
 
-							double d7 = 0.1D + k1 * 0.2D;
+							double bottomWidth = 0.1D + shells * 0.2D;
 
-							if (j == 0) {
-								d7 *= (i1 - 1) * 0.1D + 1.0D;
+							if (yPos == 0) {
+								bottomWidth *= (yPos - 1) * 0.1D + 1.0D;
 							}
 
-							for (int j1 = 0; j1 < 5; ++j1) {
-								double d8 = x + 0.5D - d6;
-								double d9 = z + 0.5D - d6;
+							topWidth *= scale16;
+							bottomWidth *= scale16;
 
-								if (j1 == 1 || j1 == 2) {
-									d8 += d6 * 2.0D;
+							for (int side = 0; side < 5; ++side) {
+								double topOffsetX = -topWidth;
+								double topOffsetZ = -topWidth;
+
+								if (side == 1 || side == 2) {
+									topOffsetX += topWidth * 2.0D;
 								}
 
-								if (j1 == 2 || j1 == 3) {
-									d9 += d6 * 2.0D;
+								if (side == 2 || side == 3) {
+									topOffsetZ += topWidth * 2.0D;
 								}
 
-								double d10 = x + 0.5D - d7;
-								double d11 = z + 0.5D - d7;
+								double bottomOffsetX = -bottomWidth;
+								double bottomOffsetZ = -bottomWidth;
 
-								if (j1 == 1 || j1 == 2) {
-									d10 += d7 * 2.0D;
+								if (side == 1 || side == 2) {
+									bottomOffsetX += bottomWidth * 2.0D;
 								}
 
-								if (j1 == 2 || j1 == 3) {
-									d11 += d7 * 2.0D;
+								if (side == 2 || side == 3) {
+									bottomOffsetZ += bottomWidth * 2.0D;
 								}
 
-								bufferbuilder.pos(d10 + d2, y + i1 * 16, d11 + d3).color(0.45F, 0.45F, 0.5F, 0.3F).endVertex();
-								bufferbuilder.pos(d8 + d4, y + (i1 + 1) * 16, d9 + d5).color(0.45F, 0.45F, 0.5F, 0.3F).endVertex();
+								bufferbuilder.pos(bottomOffsetX + topTranslateX * scale16, yPos * scale, bottomOffsetZ + topTranslateZ * scale16).color(0.45F, 0.45F, 0.5F, 0.3F).endVertex();
+								bufferbuilder.pos(topOffsetX + bottomTranslateX * scale16, (yPos + 1) * scale, topOffsetZ + bottomTranslateZ * scale16).color(0.45F, 0.45F, 0.5F, 0.3F).endVertex();
 							}
 
 							tessellator.draw();
 						}
 					}
 				}
-				GlStateManager.popMatrix();
 			}
 
 			GlStateManager.disableBlend();
 			GlStateManager.enableLighting();
 			GlStateManager.enableTexture2D();
+			GlStateManager.popMatrix();
 		}
 		GlStateManager.depthMask(true);
 
@@ -252,9 +183,13 @@ public class TESRWire extends TileEntitySpecialRenderer<TileEntityWire> {
 				this.bindTexture(ENAMEL_TEXTURE);
 				DOWN_MODEL_ENAMEL.render(ONE_SIXTEENTH);
 				this.bindTexture(texLoc);
-			}
-			if (!tileEntity.isConnectedToWire(side))
+				if (!tileEntity.isConnectedToWire(side))
+					GlStateManager.translate(SEVEN_SIXTEENTHS, 0, 0);
+			} else if (!tileEntity.isConnectedToWire(side)) {
 				GlStateManager.translate(SEVEN_SIXTEENTHS, 0, 0);
+				DOWN_MODEL_WIRE.render(ONE_SIXTEENTH);
+				GlStateManager.translate(-SEVEN_SIXTEENTHS, 0, 0);
+			}
 			DOWN_MODEL_WIRE.render(ONE_SIXTEENTH);
 			GlStateManager.popMatrix();
 		}
