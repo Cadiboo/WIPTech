892b16560d6ede6605f1fc720a66cae6a888aae7
Cadiboo <greythunder01@gmail.com>
2018-05-08 22:49:14 +1000

    Moved most rendering and math to Utils in prep for Tazers, Mounted railguns, Tesla cannons & electric lighters

--- src/main/java/cadiboo/wiptech/client/render/tileentity/TESRWire.java
+++ src/main/java/cadiboo/wiptech/client/render/tileentity/TESRWire.java
@@ -1,21 +1,18 @@
 package cadiboo.wiptech.client.render.tileentity;
 
 import java.util.List;
-import java.util.Random;
 
 import cadiboo.wiptech.WIPTech;
 import cadiboo.wiptech.block.BlockWire;
 import cadiboo.wiptech.client.model.ModelEnamel;
 import cadiboo.wiptech.client.model.ModelWire;
 import cadiboo.wiptech.tileentity.TileEntityWire;
+import cadiboo.wiptech.util.Position;
 import cadiboo.wiptech.util.Reference;
-import net.minecraft.client.renderer.BufferBuilder;
+import cadiboo.wiptech.util.Utils;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 
@@ -27,6 +24,9 @@ public class TESRWire extends TileEntitySpecialRenderer<TileEntityWire> {
 	private static final ResourceLocation ENAMEL_TEXTURE = new ResourceLocation("minecraft", "textures/blocks/concrete_brown.png");
 
 	private static final int[] RANDOMS = new int[] { 5, 6, 9, 11, 12, 14, 15, 20, 24, 25, 27, 29, 31, 37, 39, 46, 50, 51, 55, 56, 59, 61, 67, 72, 73, 74, 77, 78, 79, 83, 85, 89 };
+
+	public static final int	BRANCHES		= 1;
+	public static final int	SUB_BRANCHES	= 2;
 
 	private static final float	ONE_SIXTEENTH		= 0.0625F;
 	private static final float	SEVEN_SIXTEENTHS	= 0.4375F;
@@ -51,188 +51,44 @@ public class TESRWire extends TileEntitySpecialRenderer<TileEntityWire> {
 			WIPTech.logger.error("WIRE RENDERING ERROR! BLOCK IS NOT WIRE");
 			return;
 		}
-		final ResourceLocation texLoc = new ResourceLocation(Reference.ID, "textures/items/" + ((BlockWire) tileEntity.getBlockType()).getMetal().getName() + "_wire.png");
 		final boolean isEnamel = ((BlockWire) tileEntity.getBlockType()).isEnamel();
-		this.bindTexture(texLoc);
 
-		GlStateManager.depthMask(true);
-
-		if (tileEntity.electrocutionTime > 0) {
-			Tessellator tessellator = Tessellator.getInstance();
-			BufferBuilder bufferbuilder = tessellator.getBuffer();
-
-			final int NumberOfBranches = 1;
-			final int NumberOfPossibleSubBranches = 2;
-
+		if (!isEnamel && tileEntity.electrocutionTime > 0) {
 			List<Entity> entities = tileEntity.getElectrocutableEntities();
 			for (int i = 0; i < entities.size(); i++) {
 				GlStateManager.pushMatrix();
-				GlStateManager.disableTexture2D();
-				GlStateManager.disableLighting();
-				GlStateManager.enableBlend();
-				GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE);
 				GlStateManager.translate(x + 0.5, y + 0.5, z + 0.5);
 
-				double flyingMultiplier = 1.825;
-				double yFlying = 1.02;
-				double yAdd = 0.0784000015258789;
+				Position entityPos = Utils.getEntityRenderPos(entities.get(i), partialTicks);
+				entityPos.add(-tileEntity.getPos().getX() - 0.5, -tileEntity.getPos().getY() - 0.5, -tileEntity.getPos().getZ() - 0.5);
+				entityPos.y += entities.get(i).getEyeHeight() / 2;
 
-				if (entities.get(i) instanceof EntityPlayer && ((EntityPlayer) entities.get(i)).capabilities.isFlying) {
-					flyingMultiplier = 1.1;
-					yFlying = 1.67;
-					yAdd = 0;
-				}
-
-				double yGround = entities.get(i).motionY + yAdd == 0 && entities.get(i).prevPosY > entities.get(i).posY ? entities.get(i).posY - entities.get(i).prevPosY : 0;
-				double xFall = 1;
-				if (flyingMultiplier == 1.825)
-					if (entities.get(i).motionX != 0) {
-						if (entities.get(i).motionY + yAdd != 0) {
-							xFall = 0.6;
-						} else if (yGround != 0) {
-							xFall = 0.6;
-						}
-					} else {
-						xFall = 0.6;
-					}
-
-				double zFall = 1;
-				if (flyingMultiplier == 1.825)
-					if (entities.get(i).motionZ != 0) {
-						if (entities.get(i).motionY + yAdd != 0) {
-							zFall = 0.6;
-						} else if (yGround != 0) {
-							zFall = 0.6;
-						}
-					} else {
-						zFall = 0.6;
-					}
-
-				double dX = entities.get(i).posX - (entities.get(i).prevPosX - entities.get(i).posX) * partialTicks - (entities.get(i).motionX * xFall) * flyingMultiplier - tileEntity.getPos().getX() - 0.5;
-				double dY = entities.get(i).posY - yGround - (entities.get(i).prevPosY - entities.get(i).posY) * partialTicks - (entities.get(i).motionY + yAdd) * yFlying - tileEntity.getPos().getY() - 0.5;
-				double dZ = entities.get(i).posZ - (entities.get(i).prevPosZ - entities.get(i).posZ) * partialTicks - (entities.get(i).motionZ * zFall) * flyingMultiplier - tileEntity.getPos().getZ() - 0.5;
-				dY += entities.get(i).getEyeHeight();
-
-				double yAngle = Math.atan2(0 - dX, 0 - dZ);
+				double yAngle = Math.atan2(-entityPos.getX(), -entityPos.getZ());
 				yAngle = yAngle * (180 / Math.PI);
 				yAngle = yAngle < 0 ? 360 - (-yAngle) : yAngle;
 				GlStateManager.rotate((float) yAngle + 90, 0, 1, 0);
+
 				GlStateManager.rotate(180, 1, 0, 0);
 
-				double _Angle = Math.atan2(dY, Math.sqrt(dX * dX + dZ * dZ));
+				double _Angle = Math.atan2(entityPos.getY(), Math.sqrt(entityPos.getX() * entityPos.getX() + entityPos.getZ() * entityPos.getZ()));
 				_Angle = _Angle * (180 / Math.PI);
 				_Angle = _Angle < 0 ? 360 - (-_Angle) : _Angle;
 				GlStateManager.rotate(90 - (float) _Angle, 0, 0, 1);
 
-				final double scale = 0.125 * Math.sqrt(dX * dX + dY * dY + dZ * dZ);
-				final double scale16 = scale / 16;
+				final double scale = 0.125 * Math.sqrt(Math.pow(entityPos.getX(), 2) + Math.pow(entityPos.getY(), 2) + Math.pow(entityPos.getZ(), 2));
 
-				GlStateManager.translate(-3.5 * scale / 16, -8 * scale, -4 * scale / 16);
+				final int number = RANDOMS[(int) ((getWorld().getTotalWorldTime() + tileEntity.getPos().getX() + tileEntity.getPos().getY() + tileEntity.getPos().getZ()) % RANDOMS.length)];
 
-				final int number = RANDOMS[(int) ((getWorld().getTotalWorldTime() * scale) % RANDOMS.length)];
-
-				double[] translateXArray = new double[8];
-				double[] translateZArray = new double[8];
-				double tempX = 0.0D;
-				double tempZ = 0.0D;
-				Random random = new Random(number);
-
-				for (int counter_ = 7; counter_ >= 0; --counter_) {
-					translateXArray[counter_] = tempX;
-					translateZArray[counter_] = tempZ;
-					tempX += random.nextInt(11) - 5;
-					tempZ += random.nextInt(11) - 5;
-				}
-
-				for (int shells = 0; shells < 4; ++shells) {
-					Random random1 = new Random(number);
-					// random1 = new Random();
-					for (int branches = 0; branches < NumberOfBranches; branches++) {
-						for (int possibleSubBranches = 0; possibleSubBranches < NumberOfPossibleSubBranches + 1; ++possibleSubBranches) {
-							int position = 7;
-							int decendingHeight = 0;
-
-							if (possibleSubBranches > 0) {
-								position = 7 - possibleSubBranches;
-							}
-
-							if (possibleSubBranches > 0) {
-								decendingHeight = position - 2;
-							}
-
-							double topTranslateX = translateXArray[position];
-							double topTranslateZ = translateZArray[position];
-
-							for (int yPos = position; yPos >= decendingHeight; --yPos) {
-								double bottomTranslateX = topTranslateX;
-								double bottomTranslateZ = topTranslateZ;
-
-								if (possibleSubBranches == 0) { // Main branch
-									topTranslateX += random1.nextInt(11) - 5;
-									topTranslateZ += random1.nextInt(11) - 5;
-								} else {// Sub branch
-									topTranslateX += random1.nextInt(31) - 15;
-									topTranslateZ += random1.nextInt(31) - 15;
-								}
-
-								bufferbuilder.begin(5, DefaultVertexFormats.POSITION_COLOR);
-								double topWidth = 0.1D + shells * 0.2D;
-
-								if (yPos == 0) {
-									topWidth *= yPos * 0.1D + 1.0D;
-								}
-
-								double bottomWidth = 0.1D + shells * 0.2D;
-
-								if (yPos == 0) {
-									bottomWidth *= (yPos - 1) * 0.1D + 1.0D;
-								}
-
-								topWidth *= (scale / 16);
-								bottomWidth *= (scale / 16);
-
-								for (int side = 0; side < 5; ++side) {
-									double topOffsetX = -topWidth;
-									double topOffsetZ = -topWidth;
-
-									if (side == 1 || side == 2) {
-										topOffsetX += topWidth * 2.0D;
-									}
-
-									if (side == 2 || side == 3) {
-										topOffsetZ += topWidth * 2.0D;
-									}
-
-									double bottomOffsetX = -bottomWidth;
-									double bottomOffsetZ = -bottomWidth;
-
-									if (side == 1 || side == 2) {
-										bottomOffsetX += bottomWidth * 2.0D;
-									}
-
-									if (side == 2 || side == 3) {
-										bottomOffsetZ += bottomWidth * 2.0D;
-									}
-
-									bufferbuilder.pos(bottomOffsetX + topTranslateX * scale16, yPos * scale, bottomOffsetZ + topTranslateZ * scale16).color(0.45F, 0.45F, 0.5F, 0.3F).endVertex();
-									bufferbuilder.pos(topOffsetX + bottomTranslateX * scale16, (yPos + 1) * scale, topOffsetZ + bottomTranslateZ * scale16).color(0.45F, 0.45F, 0.5F, 0.3F).endVertex();
-								}
-
-								tessellator.draw();
-
-							}
-						}
-					}
-
-				}
-
-				GlStateManager.disableBlend();
-				GlStateManager.enableLighting();
-				GlStateManager.enableTexture2D();
+				GlStateManager.translate(0, -8 * scale, 0);
+				Utils.drawLightning(number, BRANCHES, SUB_BRANCHES, scale);
 				GlStateManager.popMatrix();
 			}
 		}
+
 		GlStateManager.depthMask(true);
+
+		final ResourceLocation texLoc = new ResourceLocation(Reference.ID, "textures/items/" + ((BlockWire) tileEntity.getBlockType()).getMetal().getName() + "_wire.png");
+		this.bindTexture(texLoc);
 
 		sides: for (int i = 0; i < EnumFacing.VALUES.length; i++) {
 			final EnumFacing side = EnumFacing.VALUES[i];
@@ -240,9 +96,7 @@ public class TESRWire extends TileEntitySpecialRenderer<TileEntityWire> {
 				continue sides;
 			GlStateManager.pushMatrix();
 			GlStateManager.translate(x + 0.5, y + 0.5, z + 0.5);
-			GlStateManager.rotate(side == EnumFacing.DOWN ? 0 : side == EnumFacing.UP ? 180F : side == EnumFacing.NORTH || side == EnumFacing.EAST ? 90F : -90F, side.getAxis() == EnumFacing.Axis.Z ? 1 : 0, 0,
-					side.getAxis() == EnumFacing.Axis.Z ? 0 : 1);
-			GlStateManager.rotate(-90, 0, 0, 1);
+			Utils.rotateHorizontal(side);
 
 			if (isEnamel) {
 				this.bindTexture(ENAMEL_TEXTURE);
--- src/main/java/cadiboo/wiptech/util/Utils.java
+++ src/main/java/cadiboo/wiptech/util/Utils.java
@@ -15,7 +15,10 @@ import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -187,6 +190,178 @@ public class Utils {
 		tessellator.draw();
 
 		GlStateManager.scale(1 / scale, 1 / scale, 1 / scale);
+	}
+
+	public static void drawLightning(final int number, final int NumberOfBranches, final int NumberOfPossibleSubBranches, final double scale) {
+		GlStateManager.depthMask(true);
+		GlStateManager.disableTexture2D();
+		GlStateManager.disableLighting();
+		GlStateManager.enableBlend();
+		GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE);
+
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder bufferbuilder = tessellator.getBuffer();
+
+		final double scale16 = scale / 16;
+
+		double[] translateXArray = new double[8];
+		double[] translateZArray = new double[8];
+		double tempX = 0.0D;
+		double tempZ = 0.0D;
+		Random random = new Random(number);
+
+		for (int counter_ = 7; counter_ >= 0; --counter_) {
+			translateXArray[counter_] = tempX;
+			translateZArray[counter_] = tempZ;
+			tempX += random.nextInt(11) - 5;
+			tempZ += random.nextInt(11) - 5;
+		}
+
+		for (int shells = 0; shells < 4; ++shells) {
+			Random random1 = new Random(number);
+			for (int branches = 0; branches < NumberOfBranches; branches++) {
+				for (int possibleSubBranches = 0; possibleSubBranches < NumberOfPossibleSubBranches + 1; ++possibleSubBranches) {
+					int position = 7;
+					int decendingHeight = 0;
+
+					if (possibleSubBranches > 0) {
+						position = 7 - possibleSubBranches;
+					}
+
+					if (possibleSubBranches > 0) {
+						decendingHeight = position - 2;
+					}
+
+					double topTranslateX = translateXArray[position];
+					double topTranslateZ = translateZArray[position];
+
+					for (int yPos = position; yPos >= decendingHeight; --yPos) {
+						double bottomTranslateX = topTranslateX;
+						double bottomTranslateZ = topTranslateZ;
+
+						if (possibleSubBranches == 0) { // Main branch
+							topTranslateX += random1.nextInt(11) - 5;
+							topTranslateZ += random1.nextInt(11) - 5;
+						} else {// Sub branch
+							topTranslateX += random1.nextInt(31) - 15;
+							topTranslateZ += random1.nextInt(31) - 15;
+						}
+
+						bufferbuilder.begin(5, DefaultVertexFormats.POSITION_COLOR);
+						double topWidth = 0.1D + shells * 0.2D;
+
+						if (yPos == 0) {
+							topWidth *= yPos * 0.1D + 1.0D;
+						}
+
+						double bottomWidth = 0.1D + shells * 0.2D;
+
+						if (yPos == 0) {
+							bottomWidth *= (yPos - 1) * 0.1D + 1.0D;
+						}
+
+						topWidth *= (scale / 16);
+						bottomWidth *= (scale / 16);
+
+						for (int side = 0; side < 5; ++side) {
+							double topOffsetX = -topWidth;
+							double topOffsetZ = -topWidth;
+
+							if (side == 1 || side == 2) {
+								topOffsetX += topWidth * 2.0D;
+							}
+
+							if (side == 2 || side == 3) {
+								topOffsetZ += topWidth * 2.0D;
+							}
+
+							double bottomOffsetX = -bottomWidth;
+							double bottomOffsetZ = -bottomWidth;
+
+							if (side == 1 || side == 2) {
+								bottomOffsetX += bottomWidth * 2.0D;
+							}
+
+							if (side == 2 || side == 3) {
+								bottomOffsetZ += bottomWidth * 2.0D;
+							}
+
+							bufferbuilder.pos(bottomOffsetX + topTranslateX * scale16, yPos * scale, bottomOffsetZ + topTranslateZ * scale16).color(0.45F, 0.45F, 0.5F, 0.3F).endVertex();
+							bufferbuilder.pos(topOffsetX + bottomTranslateX * scale16, (yPos + 1) * scale, topOffsetZ + bottomTranslateZ * scale16).color(0.45F, 0.45F, 0.5F, 0.3F).endVertex();
+						}
+
+						tessellator.draw();
+
+					}
+				}
+			}
+
+		}
+
+		GlStateManager.disableBlend();
+		GlStateManager.enableLighting();
+		GlStateManager.enableTexture2D();
+	}
+
+	public static Position getEntityRenderPos(final Entity entity, final double partialTicks) {
+		double flyingMultiplier = 1.825;
+		double yFlying = 1.02;
+		double yAdd = 0.0784000015258789;
+
+		if (entity instanceof EntityPlayer && ((EntityPlayer) entity).capabilities.isFlying) {
+			flyingMultiplier = 1.1;
+			yFlying = 1.67;
+			yAdd = 0;
+		}
+
+		double yGround = entity.motionY + yAdd == 0 && entity.prevPosY > entity.posY ? entity.posY - entity.prevPosY : 0;
+		double xFall = 1;
+		if (flyingMultiplier == 1.825)
+			if (entity.motionX != 0) {
+				if (entity.motionY + yAdd != 0) {
+					xFall = 0.6;
+				} else if (yGround != 0) {
+					xFall = 0.6;
+				}
+			} else {
+				xFall = 0.6;
+			}
+
+		double zFall = 1;
+		if (flyingMultiplier == 1.825)
+			if (entity.motionZ != 0) {
+				if (entity.motionY + yAdd != 0) {
+					zFall = 0.6;
+				} else if (yGround != 0) {
+					zFall = 0.6;
+				}
+			} else {
+				zFall = 0.6;
+			}
+
+		double dX = entity.posX - (entity.prevPosX - entity.posX) * partialTicks - (entity.motionX * xFall) * flyingMultiplier;
+		double dY = entity.posY - yGround - (entity.prevPosY - entity.posY) * partialTicks - (entity.motionY + yAdd) * yFlying;
+		double dZ = entity.posZ - (entity.prevPosZ - entity.posZ) * partialTicks - (entity.motionZ * zFall) * flyingMultiplier;
+
+		return new Position(dX, dY, dZ);
+	}
+
+	public static void rotateTowardsPos(Position origin, Position destination) {
+		double yAngle = Math.atan2(origin.getX() - destination.getX(), origin.getZ() - destination.getZ());
+		yAngle = yAngle * (180 / Math.PI);
+		yAngle = yAngle < 0 ? 360 - (-yAngle) : yAngle;
+		GlStateManager.rotate((float) yAngle + 90, 0, 1, 0);
+
+		double _Angle = Math.atan2(destination.getY(), Math.sqrt(destination.getX() * destination.getX() + destination.getZ() * destination.getZ()));
+		_Angle = _Angle * (180 / Math.PI);
+		_Angle = _Angle < 0 ? 360 - (-_Angle) : _Angle;
+		GlStateManager.rotate(90 - (float) _Angle, 0, 0, 1);
+	}
+
+	public static void rotateHorizontal(EnumFacing facing) {
+		GlStateManager.rotate(facing == EnumFacing.DOWN ? 0 : facing == EnumFacing.UP ? 180F : facing == EnumFacing.NORTH || facing == EnumFacing.EAST ? 90F : -90F, facing.getAxis() == EnumFacing.Axis.Z ? 1 : 0, 0,
+				facing.getAxis() == EnumFacing.Axis.Z ? 0 : 1);
+		GlStateManager.rotate(-90, 0, 0, 1);
 	}
 
 }
