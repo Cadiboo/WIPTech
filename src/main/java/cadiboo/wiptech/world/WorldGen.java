9ecbff1ab981930f9351fdeb38ffdea3c820a94f
Cadiboo <greythunder01@gmail.com>
2018-05-12 11:19:13 +1000

    formatting

--- src/main/java/cadiboo/wiptech/world/WorldGen.java
+++ src/main/java/cadiboo/wiptech/world/WorldGen.java
@@ -14,23 +14,19 @@ import net.minecraftforge.fml.common.IWorldGenerator;
 
 public class WorldGen implements IWorldGenerator {
 	@Override
-	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
-			IChunkProvider chunkProvider) {
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 		if (world.provider.getDimension() == 0) {
 			generateOverworld(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
 		}
 	}
 
-	private void generateOverworld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator,
-			IChunkProvider chunkProvider) {
+	private void generateOverworld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 		for (Block ore : Blocks.getOres()) {
-			generateOre(ore.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 16, 64, 4 + random.nextInt(4),
-					6);
+			generateOre(ore.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 16, 64, 4 + random.nextInt(4), 6);
 		}
 	}
 
-	private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size,
-			int chances) {
+	private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances) {
 		int deltaY = maxY - minY;
 		for (int i = 0; i < chances; i++) {
 			BlockPos pos = new BlockPos(x + random.nextInt(16), minY + random.nextInt(deltaY), z + random.nextInt(16));
