faa6187dba762a5298bcc61a76ab84e0c1b9ce59
Cadiboo <greythunder01@gmail.com>
2018-05-26 13:47:54 +1000

    formatting

--- src/main/java/cadiboo/wiptech/inventory/ContainerCrusher.java
+++ src/main/java/cadiboo/wiptech/inventory/ContainerCrusher.java
@@ -83,34 +83,28 @@ public class ContainerCrusher extends Container {
 	public ItemStack transferStackInSlot(EntityPlayer player, int index) {
 		ItemStack itemstack = ItemStack.EMPTY;
 		Slot slot = inventorySlots.get(index);
-
 		if (slot != null && slot.getHasStack()) {
 			ItemStack itemstack1 = slot.getStack();
 			itemstack = itemstack1.copy();
 
 			int containerSlots = inventorySlots.size() - player.inventory.mainInventory.size();
-
 			if (index < containerSlots) {
-				if (!this.mergeItemStack(itemstack1, containerSlots, inventorySlots.size(), true)) {
+				if (!mergeItemStack(itemstack1, containerSlots, inventorySlots.size(), true)) {
 					return ItemStack.EMPTY;
 				}
-			} else if (!this.mergeItemStack(itemstack1, 0, containerSlots, false)) {
+			} else if (!mergeItemStack(itemstack1, 0, containerSlots, false)) {
 				return ItemStack.EMPTY;
 			}
-
 			if (itemstack1.getCount() == 0) {
 				slot.putStack(ItemStack.EMPTY);
 			} else {
 				slot.onSlotChanged();
 			}
-
 			if (itemstack1.getCount() == itemstack.getCount()) {
 				return ItemStack.EMPTY;
 			}
-
 			slot.onTake(player, itemstack1);
 		}
-
 		return itemstack;
 	}
 
