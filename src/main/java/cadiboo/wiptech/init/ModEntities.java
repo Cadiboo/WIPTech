219faa75df7a3236c367abd8cc5ee80f3f49c0ad
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-08-07 15:19:33 +1000

    Removed old code & added @ObjectHolder annotations

--- src/main/java/cadiboo/wiptech/init/ModEntities.java
+++ src/main/java/cadiboo/wiptech/init/ModEntities.java
@@ -1,5 +1,15 @@
 package cadiboo.wiptech.init;
 
+import cadiboo.wiptech.util.ModReference;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
+/**
+ * Entity Instances class
+ * 
+ * @author Cadiboo
+ */
+
+@ObjectHolder(ModReference.ID)
 public class ModEntities {
 
 }
