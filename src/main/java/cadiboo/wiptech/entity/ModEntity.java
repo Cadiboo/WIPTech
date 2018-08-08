ab20fc0d73d2feeac40e5bf5f945d2ee07b2d19b
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-08-08 15:23:52 +1000

    added energy to generator & other caps to base class

--- src/main/java/cadiboo/wiptech/entity/ModEntity.java
+++ src/main/java/cadiboo/wiptech/entity/ModEntity.java
@@ -1,7 +1,17 @@
 package cadiboo.wiptech.entity;
 
+import javax.annotation.Nullable;
+
+import cadiboo.wiptech.capability.ModEnergyStorage;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.model.animation.AnimationStateMachine;
+import net.minecraftforge.common.model.animation.CapabilityAnimation;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 
 public abstract class ModEntity extends Entity {
 
@@ -9,5 +19,31 @@ public abstract class ModEntity extends Entity {
 		super(worldIn);
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
+		return this.getCapability(capability, facing) != null;
+	}
+
+	@Override
+	@Nullable
+	public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
+		if (capability == CapabilityEnergy.ENERGY)
+			return (T) this.getEnergy();
+		if (capability == CapabilityAnimation.ANIMATION_CAPABILITY)
+			return (T) this.getAnimation();
+		if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+			return (T) this.getInventory();
+		return super.getCapability(capability, facing);
+	}
+
+	@Nullable
+	public abstract ModEnergyStorage getEnergy();
+
+	@Nullable
+	public abstract AnimationStateMachine getAnimation();
+
+	@Nullable
+	public abstract IItemHandler getInventory();
 
 }
