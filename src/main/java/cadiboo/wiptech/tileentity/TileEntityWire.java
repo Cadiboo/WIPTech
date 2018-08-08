8d55633f54223efb20345fd77624d2b1f28d04d7
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-08-08 15:24:34 +1000

    changed render distance & how energy is transfered in wires

--- src/main/java/cadiboo/wiptech/tileentity/TileEntityWire.java
+++ src/main/java/cadiboo/wiptech/tileentity/TileEntityWire.java
@@ -1,11 +1,11 @@
 package cadiboo.wiptech.tileentity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import cadiboo.wiptech.capability.ModEnergyStorage;
 import cadiboo.wiptech.util.ModDamageSource;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EntitySelectors;
 import net.minecraft.util.EnumFacing;
@@ -35,39 +35,51 @@ public class TileEntityWire extends ModTileEntity implements ITickable {
 		getElectrocutableEntities().forEach((entity) -> {
 			if (shouldElectrocuteEntity(entity) && !entity.isDead)
 				if (energy.extractEnergy(getElectrocutionEnergy(), false) == getElectrocutionEnergy()) {
-					entity.attackEntityFrom(ModDamageSource.ELECTRICITY, getElectrocutionDamage());
+					entity.attackEntityFrom(ModDamageSource.ELECTRICITY.setMagicDamage(), getElectrocutionDamage());
 					entity.setFire(2);
 				}
 		});
 		if (energy.getEnergyStored() > 0)
-			for (EnumFacing face : EnumFacing.VALUES)
-				transferEnergyTowards(face);
+			transferEnergyToAllAround();
 	}
 
-	private void transferEnergyTowards(EnumFacing face) {
-		TileEntity tile = this.getWorld().getTileEntity(this.pos.offset(face));
-		if (tile == null || tile.getCapability(CapabilityEnergy.ENERGY, face.getOpposite()) == null)
-			return;
-		IEnergyStorage storage = tile.getCapability(CapabilityEnergy.ENERGY, face.getOpposite());
-		if (storage.canReceive())
-			energy.extractEnergy(storage.receiveEnergy(Math.round(energy.getEnergyStored() / EnumFacing.VALUES.length), false), false);
+	public void transferEnergyToAllAround() {
+		ArrayList<EnumFacing> connectedSides = new ArrayList<>();
+		for (EnumFacing side : EnumFacing.VALUES)
+			if (isConnectedTo(side))
+				connectedSides.add(side);
+		connectedSides.forEach(side -> {
+			IEnergyStorage storage = getWorld().getTileEntity(getPos().offset(side)).getCapability(CapabilityEnergy.ENERGY, side.getOpposite());
+			if (storage.canReceive())
+				energy.extractEnergy(storage.receiveEnergy(Math.round(energy.getEnergyStored() / connectedSides.size()), false), false);
+		});
 
 	}
+//
+//	private void transferEnergyTo(EnumFacing face) {
+//		TileEntity tile = this.getWorld().getTileEntity(this.pos.offset(face));
+//		if (tile == null || tile.getCapability(CapabilityEnergy.ENERGY, face.getOpposite()) == null)
+//			return;
+//		IEnergyStorage storage = tile.getCapability(CapabilityEnergy.ENERGY, face.getOpposite());
+//		if (storage.canReceive())
+//			energy.extractEnergy(storage.receiveEnergy(Math.round(energy.getEnergyStored() / EnumFacing.VALUES.length), false), false);
+//
+//	}
 
 	private float getElectrocutionDamage() {
-		return getElectrocutionEnergy() / 0.0001f;
+		return 1;// getElectrocutionEnergy() / 0.0001f;
 	}
 
 	private int getElectrocutionEnergy() {
-		return 1;
+		return 0;// 1;
 	}
 
 	public boolean shouldElectrocuteEntity(Entity entity) {
-		return entity instanceof EntityLivingBase && this.energy.getEnergyStored() >= getElectrocutionEnergy();
+		return entity instanceof Entity && this.energy.getEnergyStored() >= getElectrocutionEnergy();
 	}
 
 	public List<Entity> getElectrocutableEntities() {
-		return this.getWorld().getEntitiesWithinAABB(EntityLivingBase.class, new AxisAlignedBB(getPos()).grow(20 * 2), EntitySelectors.CAN_AI_TARGET);
+		return this.getWorld().getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(getPos()).grow(5), EntitySelectors.CAN_AI_TARGET);
 	}
 
 	@Override
@@ -79,6 +91,11 @@ public class TileEntityWire extends ModTileEntity implements ITickable {
 	@Override
 	public ModEnergyStorage getEnergy() {
 		return energy;
+	}
+
+	@Override
+	public double getMaxRenderDistanceSquared() {
+		return Math.pow(super.getMaxRenderDistanceSquared(), 2);
 	}
 
 }
