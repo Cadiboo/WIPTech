67344e03ddd64761dcab97a833794296faa22fdb
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-08-12 15:04:12 +1000

    added flamethrower constant

--- src/main/java/cadiboo/wiptech/init/ModItems.java
+++ src/main/java/cadiboo/wiptech/init/ModItems.java
@@ -1,5 +1,6 @@
 package cadiboo.wiptech.init;
 
+import cadiboo.wiptech.item.ItemFlamethrower;
 import cadiboo.wiptech.item.ItemPortableGenerator;
 import cadiboo.wiptech.util.ModReference;
 import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
@@ -13,6 +14,8 @@ import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 @ObjectHolder(ModReference.ID)
 public class ModItems {
 
-	public static final ItemPortableGenerator PORTABLE_GENERATOR = null;
+    public static final ItemPortableGenerator PORTABLE_GENERATOR = null;
+
+    public static final ItemFlamethrower FLAMETHROWER = null;
 
 }
