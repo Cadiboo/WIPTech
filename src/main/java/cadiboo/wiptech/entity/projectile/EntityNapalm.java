41c881e00fa7a309761aeeea944dc552fec3fa30
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-08-12 16:41:34 +1000

    formatting

--- src/main/java/cadiboo/wiptech/entity/projectile/EntityNapalm.java
+++ src/main/java/cadiboo/wiptech/entity/projectile/EntityNapalm.java
@@ -10,50 +10,49 @@ import net.minecraft.world.World;
 
 public class EntityNapalm extends EntityThrowable {
 
-    public EntityNapalm(World worldIn) {
-	super(worldIn);
-    }
-
-    public EntityNapalm(World worldIn, EntityLivingBase throwerIn) {
-	super(worldIn, throwerIn);
-	this.posY -= 0.3;
-    }
-
-    @Override
-    public void onUpdate() {
-	super.onUpdate();
-	this.igniteBlocks();
-    }
-
-    @Override
-    protected void onImpact(RayTraceResult result) {
-	if (result == null)
-	    return;
-
-	if (this.world.isRemote)
-	    return;
-
-	if (result.entityHit != null)
-	    if ((result.entityHit != getThrower()) || (this.ticksExisted > 5))
-		result.entityHit.setFire(100);
-
-	this.igniteBlocks();
-	this.setDead();
-    }
-
-    protected void igniteBlocks() {
-	if (this.world.isRemote)
-	    return;
-	BlockPos pos = new BlockPos(this.posX, this.posY, this.posZ);
-	if (world.getBlockState(pos) != Blocks.AIR.getDefaultState())
-	    return;
-
-	for (EnumFacing facing : EnumFacing.VALUES) {
-//	    if (world.getBlockState(pos.offset(facing)).getBlock().isFlammable(world, pos, facing.getOpposite()))
-	    if (world.getBlockState(pos.offset(facing)) != Blocks.AIR.getDefaultState()
-		    && world.getBlockState(pos.offset(facing)) != Blocks.FIRE.getDefaultState())
-		world.setBlockState(pos, Blocks.FIRE.getDefaultState(), 2);
+	public EntityNapalm(World worldIn) {
+		super(worldIn);
 	}
-    }
+
+	public EntityNapalm(World worldIn, EntityLivingBase throwerIn) {
+		super(worldIn, throwerIn);
+	}
+
+	@Override
+	public void onUpdate() {
+		super.onUpdate();
+		this.igniteBlocks();
+	}
+
+	@Override
+	protected void onImpact(RayTraceResult result) {
+		if (result == null)
+			return;
+
+		if (this.world.isRemote)
+			return;
+
+		if (result.entityHit != null)
+			if ((result.entityHit != getThrower()) || (this.ticksExisted > 5))
+				result.entityHit.setFire(100);
+
+		this.igniteBlocks();
+		this.setDead();
+	}
+
+	protected void igniteBlocks() {
+		if (this.world.isRemote)
+			return;
+		BlockPos pos = new BlockPos(this.posX, this.posY, this.posZ);
+		if (world.getBlockState(pos) != Blocks.AIR.getDefaultState())
+			return;
+
+		for (EnumFacing facing : EnumFacing.VALUES) {
+//	    if (world.getBlockState(pos.offset(facing)).getBlock().isFlammable(world, pos, facing.getOpposite()))
+			if (world.getBlockState(pos.offset(facing)) != Blocks.AIR.getDefaultState()
+					&& world.getBlockState(pos.offset(facing)) != Blocks.FIRE.getDefaultState())
+				world.setBlockState(pos, Blocks.FIRE.getDefaultState(), 2);
+		}
+	}
 
 }
--- src/main/java/cadiboo/wiptech/item/ItemFlamethrower.java
+++ src/main/java/cadiboo/wiptech/item/ItemFlamethrower.java
@@ -14,44 +14,44 @@ import net.minecraft.world.World;
 
 public class ItemFlamethrower extends ModItem {
 
-    public ItemFlamethrower(String name) {
-	super(name);
-	setMaxStackSize(0);
-    }
-
-    @Override
-    public int getMaxItemUseDuration(ItemStack stack) {
-	return 72000;
-    }
-
-    @Override
-    public EnumAction getItemUseAction(ItemStack stack) {
-	return EnumAction.BOW;
-    }
-
-    @Override
-    public void onUsingTick(ItemStack stack, EntityLivingBase player, int count) {
-	EntityLivingBase entityLiving = player;
-	World worldIn = player.world;
-	if ((entityLiving instanceof EntityPlayer)) {
-	    float velocity = 1F;
-	    EntityPlayer entityplayer = (EntityPlayer) entityLiving;
-	    if (!worldIn.isRemote) {
-		EntityNapalm entitynapalm = new EntityNapalm(worldIn, player);
-		entitynapalm.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, velocity, 5f);
-		worldIn.spawnEntity(entitynapalm);
-	    }
-	    worldIn.playSound(null, entityplayer.posX, entityplayer.posY, entityplayer.posZ,
-		    SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.PLAYERS, 1.0F,
-		    1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + velocity * 0.5F);
+	public ItemFlamethrower(String name) {
+		super(name);
+		setMaxStackSize(0);
 	}
-    }
 
-    @Override
-    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
-	ItemStack itemstack = playerIn.getHeldItem(handIn);
-	playerIn.setActiveHand(handIn);
-	return new ActionResult(EnumActionResult.SUCCESS, itemstack);
-    }
+	@Override
+	public int getMaxItemUseDuration(ItemStack stack) {
+		return 72000;
+	}
+
+	@Override
+	public EnumAction getItemUseAction(ItemStack stack) {
+		return EnumAction.BOW;
+	}
+
+	@Override
+	public void onUsingTick(ItemStack stack, EntityLivingBase player, int count) {
+		EntityLivingBase entityLiving = player;
+		World worldIn = player.world;
+		if ((entityLiving instanceof EntityPlayer)) {
+			float velocity = 1F;
+			EntityPlayer entityplayer = (EntityPlayer) entityLiving;
+			if (!worldIn.isRemote) {
+				EntityNapalm entitynapalm = new EntityNapalm(worldIn, player);
+				entitynapalm.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, velocity, 5f);
+				worldIn.spawnEntity(entitynapalm);
+			}
+			worldIn.playSound(null, entityplayer.posX, entityplayer.posY, entityplayer.posZ,
+					SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.PLAYERS, 1.0F,
+					1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + velocity * 0.5F);
+		}
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+		ItemStack itemstack = playerIn.getHeldItem(handIn);
+		playerIn.setActiveHand(handIn);
+		return new ActionResult(EnumActionResult.SUCCESS, itemstack);
+	}
 
 }
