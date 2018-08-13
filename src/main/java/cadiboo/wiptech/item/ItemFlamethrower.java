a2c3198a8df3934fffdaaf0dbb8018cf3038e402
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-08-13 19:25:35 +1000

    flamethrowers shoot farther & code cleanup

--- src/main/java/cadiboo/wiptech/item/ItemFlamethrower.java
+++ src/main/java/cadiboo/wiptech/item/ItemFlamethrower.java
@@ -31,20 +31,17 @@ public class ItemFlamethrower extends ModItem {
 
 	@Override
 	public void onUsingTick(ItemStack stack, EntityLivingBase player, int count) {
-		EntityLivingBase entityLiving = player;
-		World worldIn = player.world;
-		if ((entityLiving instanceof EntityPlayer)) {
-			float velocity = 1F;
-			EntityPlayer entityplayer = (EntityPlayer) entityLiving;
-			if (!worldIn.isRemote) {
-				EntityNapalm entitynapalm = new EntityNapalm(worldIn, player);
-				entitynapalm.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, velocity, 5f);
-				worldIn.spawnEntity(entitynapalm);
-			}
-			worldIn.playSound(null, entityplayer.posX, entityplayer.posY, entityplayer.posZ,
-					SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.PLAYERS, 1.0F,
-					1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + velocity * 0.5F);
+		if (!(player instanceof EntityPlayer))
+			return;
+
+		float velocity = 2F;
+		if (!player.world.isRemote) {
+			EntityNapalm entitynapalm = new EntityNapalm(player.world, player);
+			entitynapalm.shoot(player, player.rotationPitch, player.rotationYaw, 0.0F, velocity, 5.0F);
+			player.world.spawnEntity(entitynapalm);
 		}
+		player.world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ITEM_FIRECHARGE_USE,
+				SoundCategory.PLAYERS, 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + velocity * 0.5F);
 	}
 
 	@Override
