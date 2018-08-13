913907a6ef7fb0c3b32c1dfd2cb947e4f6df26e5
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-08-13 19:25:20 +1000

    debugging ore gen

--- src/main/java/cadiboo/wiptech/block/ModMaterialBlock.java
+++ src/main/java/cadiboo/wiptech/block/ModMaterialBlock.java
@@ -1,31 +1,78 @@
 package cadiboo.wiptech.block;
 
+import java.util.Random;
+
 import cadiboo.wiptech.util.ModEnums.ModMaterials;
-import cadiboo.wiptech.util.ModUtil;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public abstract class ModMaterialBlock extends ModBlock {
 
 	public ModMaterialBlock(ModMaterials materialIn, String nameSuffix) {
-		super(Material.IRON, new ResourceLocation(materialIn.getResouceLocationDomain(nameSuffix.toLowerCase(), ForgeRegistries.BLOCKS), materialIn.getNameLowercase() + "_" + nameSuffix),
-				materialIn.getNameLowercase() + "_" + nameSuffix);
+		super(Material.IRON, new ResourceLocation(materialIn.getResouceLocationDomain(nameSuffix.toLowerCase(), ForgeRegistries.BLOCKS), materialIn.getNameLowercase() + "_" + nameSuffix), materialIn
+				.getNameLowercase() + "_" + nameSuffix);
 	}
 
 	public abstract ModMaterials getModMaterial();
 
 	@Override
 	public final int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos) {
-		return ModUtil.getMaterialLightValue(this.getModMaterial());
+//		return ModUtil.getMaterialLightValue(this.getModMaterial());
+		return 14;
 	}
 
 	@Override
-	public final int getLightValue(IBlockState state) {
-		return ModUtil.getMaterialLightValue(this.getModMaterial());
+	public int getLightOpacity(IBlockState state, IBlockAccess world, BlockPos pos) {
+//		return ModUtil.getMaterialLightValue(this.getModMaterial());
+		return 0;
 	}
 
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		if (true)
+			return false;
+//		if (getModMaterial() == ModMaterials.TUNGSTEN || getModMaterial() == ModMaterials.TITANIUM || getModMaterial() == ModMaterials.TUNGSTEN)
+//			return false;
+		return super.isOpaqueCube(state);
+	}
+
+	@Override
+	public boolean isFullBlock(IBlockState state) {
+		if (true)
+			return false;
+//		if (getModMaterial() == ModMaterials.TUNGSTEN || getModMaterial() == ModMaterials.TITANIUM || getModMaterial() == ModMaterials.TUNGSTEN)
+//			return false;
+		return super.isFullBlock(state);
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		if (true)
+			return false;
+//		if (getModMaterial() == ModMaterials.TUNGSTEN || getModMaterial() == ModMaterials.TITANIUM || getModMaterial() == ModMaterials.TUNGSTEN)
+//			return false;
+		return super.isFullCube(state);
+	}
+
+	@Override
+	public boolean isTranslucent(IBlockState state) {
+		if (true)
+			return true;
+//		if (getModMaterial() == ModMaterials.TUNGSTEN || getModMaterial() == ModMaterials.TITANIUM || getModMaterial() == ModMaterials.TUNGSTEN)
+//			return true;
+		return super.isTranslucent(state);
+	}
+
+	@Override
+	public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+		super.randomDisplayTick(stateIn, worldIn, pos, rand);
+//		worldIn.createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 20, true);
+		if (getModMaterial() == ModMaterials.TUNGSTEN || getModMaterial() == ModMaterials.TITANIUM)
+			worldIn.createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 20, true);
+	}
 }
--- src/main/java/cadiboo/wiptech/world/gen/ModWorldGenerator.java
+++ src/main/java/cadiboo/wiptech/world/gen/ModWorldGenerator.java
@@ -2,31 +2,31 @@ package cadiboo.wiptech.world.gen;
 
 import java.util.Random;
 
+import cadiboo.wiptech.WIPTech;
 import cadiboo.wiptech.util.ModEnums.ModMaterials;
+import cadiboo.wiptech.util.ModUtil;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.fml.common.IWorldGenerator;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class ModWorldGenerator implements IWorldGenerator {
 
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 		switch (world.provider.getDimensionType()) {
-			case NETHER:
+		case NETHER:
 			break;
-			case OVERWORLD:
-				generateOverworld(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
+		case OVERWORLD:
+			generateOverworld(random, chunkX, chunkZ, world, chunkGenerator, chunkProvider);
 			break;
-			case THE_END:
+		case THE_END:
 			break;
-			default:
+		default:
 			break;
 
 		}
@@ -34,24 +34,96 @@ public class ModWorldGenerator implements IWorldGenerator {
 	}
 
 	private void generateOverworld(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+
+//			  this.coalGen = new WorldGenMinable(Blocks.COAL_ORE.getDefaultState(), this.chunkProviderSettings.coalSize);
+//            this.ironGen = new WorldGenMinable(Blocks.IRON_ORE.getDefaultState(), this.chunkProviderSettings.ironSize);
+//            this.goldGen = new WorldGenMinable(Blocks.GOLD_ORE.getDefaultState(), this.chunkProviderSettings.goldSize);
+//            this.redstoneGen = new WorldGenMinable(Blocks.REDSTONE_ORE.getDefaultState(), this.chunkProviderSettings.redstoneSize);
+//            this.diamondGen = new WorldGenMinable(Blocks.DIAMOND_ORE.getDefaultState(), this.chunkProviderSettings.diamondSize);
+//            this.lapisGen = new WorldGenMinable(Blocks.LAPIS_ORE.getDefaultState(), this.chunkProviderSettings.lapisSize);
+
 		for (ModMaterials material : ModMaterials.values()) {
 			if (material.getProperties().hasOre()) {
-				Block ore = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(material.getResouceLocationDomain("ore", ForgeRegistries.BLOCKS), material.getNameLowercase() + "_ore"));
-				if (ore != null)
-					generateOre(ore.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, 16, 64, 4 + random.nextInt(4), 6);
+				Block ore = material.getOre();
+				if (ore != null) {
+					if (material == ModMaterials.TUNGSTEN || material == ModMaterials.TITANIUM)
+						WIPTech.info(material, getSize(material), getChance(material));
+					generateOre(ore.getDefaultState(), world, random, chunkX * 16, chunkZ * 16, getMinY(material), getMaxY(material), getSize(material), getChance(material));
+				}
 			}
-
 		}
+	}
+
+	private int getMinY(ModMaterials material) {
+		return 4;
+	}
+
+	private int getMaxY(ModMaterials material) {
+		return Math.max(1 + getMinY(material),
+				
+				Math.round(
+						
+						Math.round(128
+								
+								* ModUtil.map(0,
+										
+										ModMaterials.getHighestHardness(), 0, 1,
+										
+										maxHardnessMinusMaterialHardness(material)
+												
+												* ModUtil.map(0,
+														
+														ModMaterials.getHighestConductivity(),
+														
+														0, 1,
+														
+														material.getProperties().getConductivity()
+												
+												)
+								
+								)
+						
+						)
+				
+				)
+		
+		);
+	}
+
+	private int getChance(ModMaterials material) {
+		int chance = Math.round(Math.round(ModUtil.map(0, ModMaterials.getHighestHardness(), 0, 10, maxHardnessMinusMaterialHardness(material))));
+		if (chance <= 1)
+			chance = 1;
+		chance += 3;
+//		chance *= 2;
+//		WIPTech.info(material, "chance: " + chance);
+		return chance;
+	}
+
+	private int getSize(ModMaterials material) {
+		int size = Math.round(Math.round(ModUtil.map(0, ModMaterials.getHighestHardness(), 0, 10, maxHardnessMinusMaterialHardness(material))));
+		if (size <= 2)
+			size = 2;
+//		WIPTech.info(material, "size: " + size);
+		return size;
+	}
+
+	private int maxHardnessMinusMaterialHardness(ModMaterials material) {
+		float highest = ModMaterials.getHighestHardness();
+		float hardness = material.getProperties().getHardness();
+
+		if (hardness == highest)
+			return 1;
+
+		return Math.round(ModMaterials.getHighestHardness()) - Math.round(material.getProperties().getHardness());
 	}
 
 	private void generateOre(IBlockState ore, World world, Random random, int x, int z, int minY, int maxY, int size, int chances) {
 		int deltaY = maxY - minY;
 		for (int i = 0; i < chances; i++) {
 			BlockPos pos = new BlockPos(x + random.nextInt(16), minY + random.nextInt(deltaY), z + random.nextInt(16));
-
 			WorldGenMinable generator = new WorldGenMinable(ore, size);
 			generator.generate(world, random, pos);
 		}
 	}
-
 }
