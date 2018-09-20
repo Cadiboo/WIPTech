c641333ef60bf70865285bf89f7e8c63d040996c
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-09-20 22:39:05 +1000

    starting on bullets

--- src/main/java/cadiboo/wiptech/entity/projectile/EntityCoilgunBullet.java
+++ src/main/java/cadiboo/wiptech/entity/projectile/EntityCoilgunBullet.java
@@ -2,6 +2,7 @@ package cadiboo.wiptech.entity.projectile;
 
 import cadiboo.wiptech.WIPTech;
 import cadiboo.wiptech.entity.IModEntity;
+import cadiboo.wiptech.util.ModDamageSource;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.util.math.RayTraceResult;
@@ -19,7 +20,17 @@ public class EntityCoilgunBullet extends EntityThrowable implements IModEntity {
 
 	@Override
 	protected void onImpact(final RayTraceResult result) {
+		if (this.world.isRemote) {
+			return;
+		}
 		WIPTech.info("EntityCoilgunBullet#onImpact");
+		if (result == null) {
+			return;
+		}
+		if (result.entityHit != null) {
+//				result.entityHit.setDead(); //this completely breaks the game bahaha
+			result.entityHit.attackEntityFrom(ModDamageSource.causeCoilgunBulletDamage(this, this.getThrower()), 10);
+		}
 
 	}
 
--- src/main/java/cadiboo/wiptech/util/ModDamageSource.java
+++ src/main/java/cadiboo/wiptech/util/ModDamageSource.java
@@ -1,9 +1,24 @@
 package cadiboo.wiptech.util;
 
+import cadiboo.wiptech.entity.projectile.EntityCoilgunBullet;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EntityDamageSourceIndirect;
 
 public class ModDamageSource {
 
 	public static final DamageSource ELECTRICITY = (new DamageSource("electricity")).setDamageIsAbsolute().setDifficultyScaled();
+
+	public static DamageSource causeCoilgunBulletDamage(final EntityCoilgunBullet coilgunBullet, final EntityLivingBase shooter) {
+		return (new EntityDamageSourceIndirect("coilgun_bullet", coilgunBullet, shooter)).setDamageIsAbsolute().setDifficultyScaled().setProjectile();
+	}
+
+//	public static DamageSource causeRailgunBulletDamage(final EntityRailgunBullet railgunBullet, final EntityLivingBase shooter) {
+//		return (new EntityDamageSourceIndirect("railgun_bullet", railgunBullet, shooter)).setDamageIsAbsolute().setDifficultyScaled().setProjectile();
+//	}
+//
+//	public static DamageSource causePlasmagunBulletDamage(final EntityPlasmagunBullet plasmagunBullet, final EntityLivingBase shooter) {
+//		return (new EntityDamageSourceIndirect("plasmagun_bullet", plasmagunBullet, shooter)).setDamageIsAbsolute().setDifficultyScaled().setProjectile().setFireDamage();
+//	}
 
 }
