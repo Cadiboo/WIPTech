89f5d1bcc807430e8cc07b5e153dd164ff2fdd4b
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-11-03 11:59:23 +1100

    Update src/main/java/cadiboo/wiptech/util/ModUtil.java

--- src/main/java/cadiboo/wiptech/util/ModUtil.java
+++ src/main/java/cadiboo/wiptech/util/ModUtil.java
@@ -41,7 +41,7 @@ public final class ModUtil {
 	 * @return the block
 	 */
 	public static Block setRegistryNames(final Block block, final ModMaterial material, final String nameSuffix) {
-		final ModResourceLocation registryName = new ModResourceLocation(material.getResouceLocationDomainWithOverrides(nameSuffix, ForgeRegistries.BLOCKS), new ModResourceLocationPath(material.getVanillaNameLowercase(nameSuffix) + (nameSuffix.length() > 0 ? "_" + nameSuffix : "")));
+		final ModResourceLocation registryName = new ModResourceLocation(material.getResouceLocationDomainWithOverrides(nameSuffix, ForgeRegistries.BLOCKS), new ModResourceLocationPath(getNameWithSuffix(material.getVanillaNameLowercase(nameSuffix), nameSuffix)));
 		block.setHardness(material.getProperties().getHardness());
 		setRegistryNames(block, registryName);
 
@@ -65,7 +65,7 @@ public final class ModUtil {
 	 * @return the item
 	 */
 	public static Item setRegistryNames(final Item item, final ModMaterial material, final String nameSuffix) {
-		final ModResourceLocation registryName = new ModResourceLocation(material.getResouceLocationDomainWithOverrides(nameSuffix, ForgeRegistries.ITEMS), new ModResourceLocationPath(material.getVanillaNameLowercase(nameSuffix) + (nameSuffix.length() > 0 ? "_" + nameSuffix : "")));
+		final ModResourceLocation registryName = new ModResourceLocation(material.getResouceLocationDomainWithOverrides(nameSuffix, ForgeRegistries.ITEMS), new ModResourceLocationPath(getNameWithSuffix(material.getVanillaNameLowercase(nameSuffix), nameSuffix)));
 		setRegistryNames(item, registryName);
 
 		final Item overriddenItem = ForgeRegistries.ITEMS.getValue(registryName);
@@ -467,6 +467,22 @@ public final class ModUtil {
 	 */
 	public static void logLogicalSide(final World world) {
 		WIPTech.info("Logical Side: " + getLogicalSide(world));
+	}
+
+	/**
+	 * "name", "suffix" -> "name_suffix"<br>
+	 * "name, "" -> "name"<br>
+	 * "", "suffix" -> "_suffix"
+	 *
+	 * @param name   the name
+	 * @param suffix the suffix
+	 * @return name + "_" + suffix if the suffix's length is greater than 0 or just the name
+	 */
+	public static String getNameWithSuffix(final String name, final String suffix) {
+		if (suffix.length() <= 0) {
+			return name;
+		}
+		return name + "_" + suffix;
 	}
 
 }
